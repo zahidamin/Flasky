82ec3a92d76716d9ac87133201a4a634fb96e6d6
zahid amin <zahidamin004@gmail.com>
2021-10-18 08:39:14 +0500

    Flask folder is created for practice.

---
+++ hello3.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+from flask import request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
